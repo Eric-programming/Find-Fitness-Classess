a3477f2233273415ad3343a490c33d65e46eff8d
eric-programming <eric09279@outlook.com>
2020-05-15 00:04:37 -0500

    update

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,43 +6,45 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Persistance;
 
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace API {
+    public class Startup {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddDbContext<DataContext>(optionsAction=> {
-                optionsAction.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddControllers ();
+            services.AddDbContext<DataContext> (optionsAction => {
+                optionsAction.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
+            });
+            services.AddCors (options => {
+                options.AddPolicy (name: MyAllowSpecificOrigins,
+                    builder => {
+                        builder.WithOrigins ("http://localhost:3000").AllowAnyHeader ()
+                            .AllowAnyMethod ();
+                    });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
             // app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseAuthorization ();
+            app.UseCors (MyAllowSpecificOrigins);
+            // app.UseCors (x => x.AllowAnyOrigin ().AllowAnyHeader ().AllowAnyMethod ());
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const http = "http://localhost:4000/api/values";
 function App() {
+  const [value, setValue] = useState<Object[]>([]);
+  useEffect(() => {
+    axios.get(http).then((res) => {
+      console.log("res", res);
+      // setValue()
+    });
+    setValue([1, 2]);
+  }, []);
   return (
-    <></>
+    <>
+      <h2>{value}</h2>
+    </>
   );
 }
 
