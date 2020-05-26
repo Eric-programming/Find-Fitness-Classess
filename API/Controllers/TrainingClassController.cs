314ea80f5f10682bf0341dbe4223bb15efaf474e
eric-programming <eric09279@outlook.com>
2020-05-25 19:32:31 -0500

    update

--- API/Controllers/TrainingClassController.cs
+++ API/Controllers/TrainingClassController.cs
@@ -10,29 +10,21 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TrainingClassController : ControllerBase
+    public class TrainingClassController : BaseController
     //Use ControllerBase if no view
     {
-        private readonly IMediator _mediator;
-
-
-        public TrainingClassController(IMediator mediator)
-        {
-            _mediator = mediator;
-
-        }
         // GET api/trainingclass
         [HttpGet]
         public async Task<ActionResult<List<TrainingClass>>> GetTrainingClassess()
         {
-            var trainingclasses = await _mediator.Send(new List.Query());
+            var trainingclasses = await Mediator.Send(new List.Query());
 
             return Ok(trainingclasses);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<TrainingClass>> GetTrainingClass(Guid id)
         {
-            var trainingclass = await _mediator.Send(new ListDetail.Query { Id = id });
+            var trainingclass = await Mediator.Send(new ListDetail.Query { Id = id });
             if (trainingclass == null)
                 return NotFound();
             return Ok(trainingclass);
@@ -40,19 +32,19 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<Unit>> CreateTrainingClass(Create.Command command)
         {
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
         [HttpPut("{id}")]
         public async Task<ActionResult<Unit>> EditTrainingClass(Guid id, Edit.Command command)
         {
             command.Id = id;
-            return await _mediator.Send(command);
+            return await Mediator.Send(command);
         }
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> DeleteTrainingClass(Guid id)
         {
 
-            return await _mediator.Send(new Delete.Command { Id = id });
+            return await Mediator.Send(new Delete.Command { Id = id });
         }
     }
 }
