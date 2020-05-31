473af3dea24d6b55625ad7e8405f7f9810afe6db
eric-programming <eric09279@outlook.com>
2020-05-31 13:58:12 -0500

    fix set up

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,6 +4,8 @@ using Application.TrainingClasses;
 using API.Middleware;
 using AutoMapper;
 using Domain;
+using infrastructure.Security;
+using Infrastructure.Photo;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -15,27 +17,20 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Persistance;
-using Infrastructure.Security;
-using infrastructure.Security;
 
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace API {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddDbContext<DataContext>(optionsAction =>
-            {
-                optionsAction.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddControllers ();
+            services.AddDbContext<DataContext> (optionsAction => {
+                optionsAction.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
             });
             // services.AddCors(options =>
             // {
@@ -46,53 +41,49 @@ namespace API
             //                 .AllowAnyMethod();
             //         });
             // });
-            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader()));
+            services.AddCors (options => options.AddPolicy ("AllowAll", p => p.AllowAnyOrigin ()
+                .AllowAnyMethod ()
+                .AllowAnyHeader ()));
 
-            services.AddMediatR(typeof(List.Handler).Assembly); //Just one handler is good
-            services.AddAutoMapper(typeof(List.Handler).Assembly); //It will take a look at the application folder
-            var builder = services.AddIdentityCore<User>();
-            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
-            identityBuilder.AddEntityFrameworkStores<DataContext>();
-            identityBuilder.AddSignInManager<SignInManager<User>>();
+            services.AddMediatR (typeof (List.Handler).Assembly); //Just one handler is good
+            services.AddAutoMapper (typeof (List.Handler).Assembly); //It will take a look at the application folder
+            var builder = services.AddIdentityCore<User> ();
+            var identityBuilder = new IdentityBuilder (builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<DataContext> ();
+            identityBuilder.AddSignInManager<SignInManager<User>> ();
             //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt => opt.TokenValidationParameters = new TokenValidationParameters
-            {
+            var key = new SymmetricSecurityKey (Encoding.UTF8.GetBytes (Configuration["TokenKey"]));
+            services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme).AddJwtBearer (opt => opt.TokenValidationParameters = new TokenValidationParameters {
                 ValidateIssuerSigningKey = true,
-                IssuerSigningKey = key,
-                ValidateAudience = false,
-                ValidateIssuer = false
+                    IssuerSigningKey = key,
+                    ValidateAudience = false,
+                    ValidateIssuer = false
             });
 
-            services.AddScoped<IJWTGen, JWTGen>(); //Injectiable
-            services.AddScoped<IUserAccessor, UserAccessor>(); //Injectiable
+            services.AddScoped<IJWTGen, JWTGen> (); //Injectiable
+            services.AddScoped<IUserAccessor, UserAccessor> (); //Injectiable
 
             //Cloudary ⬇️
-            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+            services.Configure<CloudinarySetting> (Configuration.GetSection ("CloudinarySettings"));
             //Cloundary⬆️
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseMiddleware<ErrorHandlingMiddleware>();
-            if (env.IsDevelopment())
-            {
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseMiddleware<ErrorHandlingMiddleware> ();
+            if (env.IsDevelopment ()) {
                 // app.UseDeveloperExceptionPage();
             }
             // app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseCors("AllowAll");
+            app.UseRouting ();
+            app.UseCors ("AllowAll");
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+            app.UseAuthentication ();
+            app.UseAuthorization ();
             // app.UseCors (MyAllowSpecificOrigins);
             // app.UseCors (x => x.AllowAnyOrigin ().AllowAnyHeader ().AllowAnyMethod ());
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
--- infrastructure/Image/CloudinarySetting.cs
+++
@@ -1,9 +0,0 @@
-namespace infrastructure.Image
-{
-    public class CloudinarySetting
-    {
-        public string CloudName { get; set; }
-        public string ApiKey { get; set; }
-        public string ApiSecret { get; set; }
-    }
-}
